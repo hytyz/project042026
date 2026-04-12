233698df977972f00fa9c2c75acd6c2dfa5a5f16
hytyz <pomelyantseva@my.bcit.ca>
2026-04-12 13:20:00 -0700

    fix big with login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\SetController;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
@@ -9,6 +11,24 @@ Route::inertia('/', 'welcome', [
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+
+    // Flashcard routes
+    Route::inertia('flashcards/review', 'flashcards/review')->name('flashcards.review');
+
+    // Sets routes
+    Route::inertia('sets', 'sets/index')->name('sets.index');
+    Route::get('sets/{set}', [SetController::class, 'show'])->name('sets.show');
+    Route::get('sets/{set}/practice', [SetController::class, 'practice'])->name('sets.practice');
+});
+
+Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('index');
+    Route::get('/users', [AdminController::class, 'users'])->name('users');
+    Route::get('/users/{user}', [AdminController::class, 'userDetails'])->name('users.show');
+    Route::delete('/users/{user}', [AdminController::class, 'deleteUser'])->name('users.destroy');
+    Route::get('/sets', [AdminController::class, 'sets'])->name('sets');
+    Route::get('/sets/{set}', [AdminController::class, 'setDetails'])->name('sets.show');
+    Route::delete('/sets/{set}', [AdminController::class, 'deleteSet'])->name('sets.destroy');
 });
 
 require __DIR__.'/settings.php';
