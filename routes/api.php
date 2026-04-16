16885a1a9d09cea698c09b9a15a56437b74ce3eb
Polina Omelyantseva <pomelyantseva@my.bcit.ca>
2026-04-16 00:34:28 -0700

    attempt to fix api

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Api\FlashcardSetController;
 use App\Http\Controllers\Api\TagController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['web', 'auth'])->prefix('api')->name('api.')->group(function () {
+Route::middleware('auth')->name('api.')->group(function () {
     Route::post('/flashcards/generate', [FlashcardController::class, 'generate'])->name('flashcards.generate');
 
     Route::apiResource('sets', FlashcardSetController::class);
