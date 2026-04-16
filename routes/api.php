4714e9603ab0ff60995920d5464bb07238f5e22c
Polina Omelyantseva <pomelyantseva@my.bcit.ca>
2026-04-15 20:30:59 -0700

    chage to api.php

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\Api\FlashcardSetController;
 use App\Http\Controllers\Api\TagController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['web', 'auth'])->group(function () {
-    Route::post('/flashcards/generate', [FlashcardController::class, 'generate']);
+Route::middleware(['web', 'auth'])->prefix('api')->name('api.')->group(function () {
+    Route::post('/flashcards/generate', [FlashcardController::class, 'generate'])->name('flashcards.generate');
 
     Route::apiResource('sets', FlashcardSetController::class);
 
